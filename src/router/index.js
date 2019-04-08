7401a6dfc6086ac62f6d9d7177e0a10984bd5b6e
agan <814215920@qq.com>
2019-04-08 21:02:02 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../pages/Home/Home'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
